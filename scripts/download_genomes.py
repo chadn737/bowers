5bd1c43ba277782f297685bfe7422c183300ee4c
Chad <chadn737@gmail.com>
2017-06-24 21:51:29 -0400

    Updating Ptrichocarpa

--- scripts/download_genomes.py
+++ scripts/download_genomes.py
@@ -6,7 +6,7 @@ files = {
 'Bdistachyon.fa':'https://genomevolution.org/coge/api/v1/genomes/25040/sequence',
 'Osativa.fa':'https://genomevolution.org/coge/api/v1/genomes/29143/sequence',
 'Ppersica.fa':'https://genomevolution.org/coge/api/v1/genomes/34844/sequence',
-'Ptrichocarpa.fa':'https://genomevolution.org/coge/api/v1/genomes/22743/sequence',
+'Ptrichocarpa.fa':'https://genomevolution.org/coge/api/v1/genomes/23993/sequence',
 'Sbicolor.fa':'https://genomevolution.org/coge/api/v1/genomes/31607/sequence',
 'Slycopersicum.fa':'https://genomevolution.org/coge/api/v1/genomes/24769/sequence',
 'Vvinifera.fa':'https://genomevolution.org/coge/api/v1/genomes/19990/sequence',
